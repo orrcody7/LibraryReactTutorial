e32e224904a73478505be0fc67dc7bb8886cb25d
unknown <orrcody7@gmail.com>
2021-09-09 13:48:57 -0600

    created and completed the createBook component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css'
 import TitleBar from './TitleBar/TitleBar';
 import BookViewer from './BookViewer/BookViewer';
+import BookCreator from './BookCreator/BookCreator';
 
 class App extends Component {
     constructor(props){
@@ -38,12 +39,20 @@ class App extends Component {
         });
     }
 
+    createBook = (newBook) => {
+        console.log(newBook);
+        this.books.push(newBook);
+        this.setState({
+            bookNumber: this.books.length -1
+        })
+    }
+
     render() {
         return (
         <div className="container-fluid">
             <TitleBar />
             <BookViewer book={this.books[this.state.bookNumber]} nextBook={this.goToNextBook} previousBook={this.goToPreviousBook} />
-
+            <BookCreator createNewBook={this.createBook} />
         </div>
         )
     }
